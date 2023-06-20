3b909f2e04f8df40232f635c2eb0caf22c8ba719
Enzo_Gatica <enzogatica96@gmail.com>
2023-06-19 22:15:42 -0400

    CheckPoint, datos tomados con Toast

--- app/src/main/java/com/example/ingresoinformacioni6m4/MainActivity.java
+++ app/src/main/java/com/example/ingresoinformacioni6m4/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.ingresoinformacioni6m4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText textName;
+        EditText textLastName;
+        EditText textMail;
+        EditText textPass;
+
+        textName = findViewById(R.id.textName);
+        textLastName = findViewById(R.id.textLastName);
+        textMail = findViewById(R.id.textMail);
+        textPass = findViewById(R.id.textPass);
+
+        Button btnCreate = findViewById(R.id.btnCreate);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = textName.getText().toString();
+                String lastName = textLastName.getText().toString();
+                String email = textMail.getText().toString();
+                String pass = textPass.getText().toString();
+
+                String texto = "Usuario" + name +" "+lastName + " Email: "+email+" Contraseña: "+ pass;
+
+                Toast.makeText(getBaseContext(), texto, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
     }
 }
